d87f6bbb03fe969ed2cf1739fd84da95e1efd32f
Elant-debug <76825872+Elant-debug@users.noreply.github.com>
2025-12-26 17:27:16 +0000

    Added age and color

--- Person.java
+++ Person.java
@@ -1,18 +1,36 @@
 public class Person {
   private String name;
-
-  public Person(String name) {
-    this.name = name;
+  private int age;
+  public String color;
+  
+  public Person(String moniker, int age, String color) {
+    name = moniker;
+    this.age = age;
+    this.color = color;
   }
 
+  public void setName (String name) {
+    this.name = name;
+  }
   public String getName() {
     return name;
+  }
+  public int getAge () {
+    return age;
   }
 
   public static void main(String[] args) {
     System.out.println("Hello world");
-    Person person = new Person("John Doe");
+    Person guy = new Person("John Doe", 18, "blue");
 
-    System.out.println("Person's name: " + person.getName());
+    System.out.println("Person's name: " + guy.getName());
+
+    System.out.println("Person's Age: " + guy.getAge());
+    
+    System.out.println("Favorite Color: " + guy.color);
+
+    guy.setName( "Mike");
+    System.out.println("Person's name: " + guy.getName());
+
   }
 }
