ff46659c9de2e6fdab6cd3614f45d974e6845459
johnfabrycky <157093716+johnfabrycky@users.noreply.github.com>
2025-12-26 17:10:15 +0000

    added Person constructor and print name in main method

--- Person.java
+++ Person.java
@@ -1,7 +1,18 @@
-public class Person(
+public class Person {
   private String name;
+
+  public Person(String name) {
+    this.name = name;
+  }
 
   public String getName() {
     return name;
   }
-)
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+    Person person = new Person("John Doe");
+
+    System.out.println("Person's name: " + person.getName());
+  }
+}
